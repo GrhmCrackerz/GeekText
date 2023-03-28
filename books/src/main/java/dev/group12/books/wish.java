b43f3d6e27c037b74f9e702717afed03f04072ce
Markrides <magutierrez1161@hotmail.com>
2023-03-27 20:33:17 -0400

    Update wish.java

--- books/src/main/java/dev/group12/books/wish.java
+++ books/src/main/java/dev/group12/books/wish.java
@@ -3,12 +3,9 @@ package dev.group12.books;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
-
-import java.util.List;
 
 @Document(collection = "books")
 @Data
